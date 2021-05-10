a0931e103e741d7787062a2e03b33f1b0e855f33
Péter Hegedűs <hpeti0912@gmail.com>
2021-05-10 14:36:46 +0200

    Implemented Direction interface in this class, with 4 basic moves

---
+++ src/main/java/boardgame/model/SimpleDirection.java
@@ -0,0 +1,42 @@
+package boardgame.model;
+
+import java.util.Arrays;
+
+public enum SimpleDirection implements Direction {
+
+    UP(-1, 0),
+    RIGHT(0, 1),
+    DOWN(1, 0),
+    LEFT(0, -1);
+
+    private final int rowChange;
+    private final int colChange;
+
+    SimpleDirection(int rowChange, int colChange) {
+        this.rowChange = rowChange;
+        this.colChange = colChange;
+    }
+
+    public int getRowChange() {
+        return rowChange;
+    }
+
+    public int getColChange() {
+        return colChange;
+    }
+
+    public static SimpleDirection of(int rowChange, int colChange) {
+        for (var direction : values()) {
+            if (direction.rowChange == rowChange && direction.colChange == colChange) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(of(1, 0));
+        Arrays.stream(values()).map(x -> x.colChange).forEach(System.out::println);
+    }
+
+}
