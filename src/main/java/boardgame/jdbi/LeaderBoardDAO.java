451a70150bd409d932425304d20fb0e87cf3f09b
HPeti <hpeti0912@gmail.com>
2021-05-17 01:22:31 +0200

    Created DAO interface for LeaderBoard

---
+++ src/main/java/boardgame/jdbi/LeaderBoardDAO.java
@@ -0,0 +1,44 @@
+package boardgame.jdbi;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+@RegisterBeanMapper(PlayerInfo.class)
+public interface LeaderBoardDAO {
+    @SqlUpdate("""
+            create table LeaderBoard(
+                name varchar2 primary key,
+                winCount int not null,
+                loseCount int not null)
+            """)
+    void createTable();
+
+    @SqlUpdate("insert into LeaderBoard values(:name, 1, 0)")
+    void insertWinnerPlayer(@Bind("name") String name);
+
+    @SqlUpdate("insert into LeaderBoard values(:name, 0, 1)")
+    void insertLoserPlayer(@Bind("name") String name);
+
+    @SqlUpdate("update LeaderBoard set winCount = winCount + 1 where name == :name ")
+    void incrementPlayerWins(@Bind("name") String name);
+
+    @SqlUpdate("update LeaderBoard set loseCount = loseCount + 1 where name == :name ")
+    void incrementPlayerLoses(@Bind("name") String name);
+
+    @SqlQuery("select winCount from LeaderBoard where name == :name")
+    int numberOfPlayerWins(@Bind("name") String name);
+
+    @SqlQuery("select loseCount from LeaderBoard where name == :name")
+    int numberOfPlayerLoses(@Bind("name") String name);
+
+    @SqlQuery("select name from LeaderBoard order by winCount desc, loseCount ")
+    PlayerInfo[] getPlayersOrdered();
+
+    @SqlQuery("select exists (select * from LeaderBoard where name = :name")
+    boolean playerExists(@Bind("name")String name);
+
+    @SqlUpdate("delete from LeaderBoard")
+    void deletePlayerInfos();
+}
