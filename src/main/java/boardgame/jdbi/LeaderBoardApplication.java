251cdfc14d25f32eb05d9e74e58ad3e9a9d57f0a
HPeti <hpeti0912@gmail.com>
2021-05-17 01:53:51 +0200

    Added Application for loading in LeaderBoard.fxml

---
+++ src/main/java/boardgame/jdbi/LeaderBoardApplication.java
@@ -0,0 +1,21 @@
+package boardgame.jdbi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LeaderBoardApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LeaderBoard.fxml"));
+        stage.setTitle("JavaFX Board Game Project: Leaderboard");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
