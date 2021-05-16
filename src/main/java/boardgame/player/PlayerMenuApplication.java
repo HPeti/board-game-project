6331214a8de4e9ca89fdeb627d5471b360ca70b6
HPeti <hpeti0912@gmail.com>
2021-05-16 23:17:22 +0200

    Added Application class for loading Player menu

---
+++ src/main/java/boardgame/player/PlayerMenuApplication.java
@@ -0,0 +1,21 @@
+package boardgame.player;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlayerMenuApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/PlayerMenu.fxml"));
+        stage.setTitle("JavaFX Board Game Project");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
