a9d542f291f7a0bc37c0cb04f56454cb550c3c11
HPeti <hpeti0912@gmail.com>
2021-05-17 09:07:15 +0200

    Created checkConnection for checking if jdbi is initialized or not. It will be called before every operation.

--- src/main/java/boardgame/jdbi/LeaderBoardHandler.java
+++ src/main/java/boardgame/jdbi/LeaderBoardHandler.java
@@ -1,5 +1,6 @@
 package boardgame.jdbi;
 
+import lombok.extern.java.Log;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.h2.H2DatabasePlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -10,59 +11,81 @@ import java.util.ArrayList;
 public class LeaderBoardHandler {
     private static Jdbi jdbi;
 
-    public LeaderBoardHandler(){
-        jdbi = Jdbi.create("jdbc:h2:file:~/.boardgame-2_19/LeaderBoard","as","");
+    public LeaderBoardHandler() {
+        makeConnection();
+        tryCreateTable();
+    }
+
+    public void makeConnection() {
+        jdbi = Jdbi.create("jdbc:h2:file:~/.boardgame-2_19/LeaderBoard", "as", "");
         jdbi.installPlugin(new SqlObjectPlugin());
         jdbi.installPlugin(new H2DatabasePlugin());
+    }
+
+    public void tryCreateTable() {
         try {
-            jdbi.withExtension(LeaderBoardDAO.class, dao -> {dao.createTable(); return true;});
-        }
-        catch (Exception exception){
-            Logger.debug("Table already exists! ("+exception.getClass()+")");
+            jdbi.withExtension(LeaderBoardDAO.class, dao -> {
+                dao.createTable();
+                return true;
+            });
+        } catch (Exception exception) {
+            Logger.debug("Table already exists! (" + exception.getClass() + ")");
         }
     }
 
-    public static void updateWins(String name){
-        boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
-        if (playerExists){
-            jdbi.withExtension(LeaderBoardDAO.class, dao ->{ dao.incrementPlayerWins(name); return true;});
-        }
-        else {
-            jdbi.withExtension(LeaderBoardDAO.class, dao ->{ dao.insertWinnerPlayer(name); return true;});
-        }
-    }
-    public static void updateLoses(String name){
-        boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
-        if (playerExists){
-            jdbi.withExtension(LeaderBoardDAO.class, dao ->{ dao.incrementPlayerLoses(name); return true;});
-        }
-        else {
-            jdbi.withExtension(LeaderBoardDAO.class, dao ->{ dao.insertLoserPlayer(name);return true;});
+    public static void checkConnection() {
+        Logger.debug("Checking jdbi connection...");
+        if (jdbi == null) {
+            new LeaderBoardHandler();
+            Logger.debug("Created new connection");
+        } else {
+            Logger.debug("Connection is already up.");
         }
     }
-    public static ArrayList<PlayerInfo> getFirstTenPlayerInfos(){
-        if (jdbi == null){
-            ArrayList<PlayerInfo> topTen = new ArrayList<PlayerInfo>();
-            Logger.debug("nincs jdbi");
-            return topTen;
+
+    public static void updateWins(String name) {
+        checkConnection();
+        boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
+        if (playerExists) {
+            jdbi.withExtension(LeaderBoardDAO.class, dao -> {
+                dao.incrementPlayerWins(name);
+                return true;
+            });
+        } else {
+            jdbi.withExtension(LeaderBoardDAO.class, dao -> {
+                dao.insertWinnerPlayer(name);
+                return true;
+            });
         }
-        else {
-            PlayerInfo[] playerInfos = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.getPlayersOrdered());
-            /*
-            String[] playerNames = jdbi.withExtension(BoardSetDao.class, dao -> dao.getFirstTenName());
-            int[] winnerScores = jdbi.withExtension(BoardSetDao.class, dao -> dao.getFirstTenWinCount());
-            int[] losesScores = jdbi.withExtension(BoardSetDao.class, dao -> dao.getFirstTenLosesCount());
-            int[] drawsScores = jdbi.withExtension(BoardSetDao.class, dao -> dao.getFirstTenDrawsCount());
-            */
-            int topCount = 10;
-            ArrayList<PlayerInfo> topTen = new ArrayList<PlayerInfo>();
-            if (playerInfos.length < 10) {
-                topCount = playerInfos.length;
-            }
-            for (int i = 0; i < topCount; i++) {
-                topTen.add(playerInfos[i]);
-            }
-            return topTen;
+    }
+
+    public static void updateLoses(String name) {
+        checkConnection();
+        boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
+        if (playerExists) {
+            jdbi.withExtension(LeaderBoardDAO.class, dao -> {
+                dao.incrementPlayerLoses(name);
+                return true;
+            });
+        } else {
+            jdbi.withExtension(LeaderBoardDAO.class, dao -> {
+                dao.insertLoserPlayer(name);
+                return true;
+            });
         }
+    }
+
+    public static ArrayList<PlayerInfo> getFirstTenPlayerInfos() {
+        checkConnection();
+        PlayerInfo[] playerInfos = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.getPlayersOrdered());
+        int topCount = 10;
+        ArrayList<PlayerInfo> topTen = new ArrayList<PlayerInfo>();
+        if (playerInfos.length < 10) {
+            topCount = playerInfos.length;
+        }
+        for (int i = 0; i < topCount; i++) {
+            topTen.add(playerInfos[i]);
+        }
+        return topTen;
     }
 }
