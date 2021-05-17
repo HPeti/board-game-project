e68906f86bfdc4c33b7fac268d15a045e248f2ed
HPeti <hpeti0912@gmail.com>
2021-05-17 10:33:33 +0200

    Added javadoc to class

--- src/main/java/boardgame/jdbi/PlayerInfo.java
+++ src/main/java/boardgame/jdbi/PlayerInfo.java
@@ -4,11 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This class is made for storing info about a player.
+ * This class uses lombok to create constructor for each field.
+ * They are used at binding at {@code LeaderBoardDAO} class.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class PlayerInfo {
+    /**
+     * Stores info about Player's name
+     */
     private String name;
+    /**
+     * Stores how many times a Player won.
+     */
     private int winCount;
+    /**
+     * Stores how many times a Player lost.
+     */
     private int loseCount;
 }
