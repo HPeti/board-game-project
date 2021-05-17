aa89347faa8b9043cb8dd20e6d7981cdac7d9d18
HPeti <hpeti0912@gmail.com>
2021-05-17 10:34:48 +0200

    Created javadoc for class

--- src/main/java/boardgame/jdbi/LeaderBoardHandler.java
+++ src/main/java/boardgame/jdbi/LeaderBoardHandler.java
@@ -1,6 +1,5 @@
 package boardgame.jdbi;
 
-import lombok.extern.java.Log;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.h2.H2DatabasePlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -8,20 +7,39 @@ import org.tinylog.Logger;
 
 import java.util.ArrayList;
 
+/**
+ * This class handles JDBI connections and operations for {@code LeaderBoardController}.
+ */
 public class LeaderBoardHandler {
+    /**
+     * This field is used to store the JDBI's state
+     */
     private static Jdbi jdbi;
 
+    /**
+     * Default constructor for {@code LeaderBoardHandler} class
+     * It initializes a connection, and tries to create a table to store data.
+     */
     public LeaderBoardHandler() {
         makeConnection();
         tryCreateTable();
     }
 
+    /**
+     * Makes JDBI connection.
+     * Creates a path to the given path, and installs plugins for later usage.
+     */
     public void makeConnection() {
         jdbi = Jdbi.create("jdbc:h2:file:~/.boardgame-2_19/LeaderBoard", "as", "");
         jdbi.installPlugin(new SqlObjectPlugin());
         jdbi.installPlugin(new H2DatabasePlugin());
     }
 
+    /**
+     * Tries to create a table.
+     * If LeaderBoard's table doesn't exists, it creates it.
+     * If it does exists, then logs a line.
+     */
     public void tryCreateTable() {
         try {
             jdbi.withExtension(LeaderBoardDAO.class, dao -> {
@@ -33,6 +51,10 @@ public class LeaderBoardHandler {
         }
     }
 
+    /**
+     * Checks whether JDBI connection is initialized or not.
+     * If it's not initialized, then it initializes it.
+     */
     public static void checkConnection() {
         Logger.debug("Checking jdbi connection...");
         if (jdbi == null) {
@@ -43,6 +65,13 @@ public class LeaderBoardHandler {
         }
     }
 
+    /**
+     * Stores a win for the given Player's name.
+     * It checks whether the Player's name exists in the database.
+     * If it exists, it will increase {@code winCount} by one.
+     * If it does not exists, it will set {@code winCount} to one.
+     * @param name is given as an ID in the database.
+     */
     public static void updateWins(String name) {
         checkConnection();
         boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
@@ -59,6 +88,13 @@ public class LeaderBoardHandler {
         }
     }
 
+    /**
+     * Stores a lose for the given Player's name.
+     * It checks whether the Player's name exists in the database.
+     * If it exists, it will increase {@code loseCount} by one.
+     * If it does not exists, it will set {@code loseCount} to one.
+     * @param name is given as an ID in the database.
+     */
     public static void updateLoses(String name) {
         checkConnection();
         boolean playerExists = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.playerExists(name));
@@ -75,6 +111,11 @@ public class LeaderBoardHandler {
         }
     }
 
+    /**
+     * Gets the first 10 {@code PlayerInfo} data from the database.
+     * If 10 {@code PlayerInfo} is not stored, then return as many it exists.
+     * @return an {@code ArrayList} which stores {@code PlayerInfo}s.
+     */
     public static ArrayList<PlayerInfo> getFirstTenPlayerInfos() {
         checkConnection();
         PlayerInfo[] playerInfos = jdbi.withExtension(LeaderBoardDAO.class, dao -> dao.getPlayersOrdered());
